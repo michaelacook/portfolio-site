2a1db8643456f90a6b76a23c05bb0b64fb4f6ddb
Michael Cook <mcook0775@gmail.com>
2020-04-26 18:34:08 -0400

    Fifth commit

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-/* Entry point */
+
+/*
+Instantiate the app, require dependencies, set up middleware and routes.
+Entry point is bin/www.js
+*/
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8000;
 const routes = require('./routes/routes.js');
 const projectsMiddleware = require('./middleware/projectsMiddleware.js');
 
 app.set('view engine', 'pug');
-
 
 // middleware
 app.use('/static', express.static('public'));
 app.use((req, res, next) => projectsMiddleware(req, res, next));
 app.use(express.urlencoded());
 app.use(routes);
-app.listen(port);
+
+module.exports = app;
